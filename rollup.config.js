881fb8e45589b8a84b2e7ab63f3b0f85b0d04bc3
khalid ait bellahs <kaitbellahs@gmail.com>
2022-10-03 11:21:34 -0300

    Resolve rollup bug, not recognize decalaration out dir

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import autoExternal from 'rollup-plugin-auto-external';
+import tsPlugin from '@rollup/plugin-typescript';
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import pkg from "./package.json";
+import tsconfig from './tsconfig.json';
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        },
+    ],
+    plugins: [
+        autoExternal(),
+        tsPlugin(tsconfig),
+        resolve({
+            preferBuiltins: true
+        }),
+        json(),
+        terser()
+    ],
+}

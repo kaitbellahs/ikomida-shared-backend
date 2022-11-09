2d3680eefaa3486171d26d9fa3e307f35d84e384
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-11-09 18:55:00 -0300

    Update libs
    Format code

--- src/Types/CGeometry.ts
+++ src/Types/CGeometry.ts
@@ -2,13 +2,13 @@ import { Classes, Decorators, Types } from '@ikomida/shared-types'
 import TGeometry from './TGeometry.js'
 
 export default class CGeometry extends Classes.BaseJSON {
-    @Decorators.Property.Property
-    type?: TGeometry
-    @Decorators.Property.Property
-    coordinates?: number[]
+  @Decorators.Property.Property
+  type?: TGeometry
+  @Decorators.Property.Property
+  coordinates?: number[]
 
-    static init(type?: TGeometry, coordinates?: number[], id?: string, timestamp?: number): CGeometry {
-        // eslint-disable-next-line prefer-rest-params
-        return this.createInitObject(arguments, ['type', 'coordinates', 'id', 'timestamp'])
-    }
+  static init(type?: TGeometry, coordinates?: number[], id?: string, timestamp?: number): CGeometry {
+    // eslint-disable-next-line prefer-rest-params
+    return this.createInitObject(arguments, ['type', 'coordinates', 'id', 'timestamp'])
+  }
 }

8840a4be253bd524d3551975bf258c23453727fc
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-11-09 17:04:20 -0300

    Bugs fix

--- src/Types/CGeometry.ts
+++ src/Types/CGeometry.ts
@@ -1,14 +1,14 @@
 import { Classes, Decorators, Types } from '@ikomida/shared-types'
-import { TGeometry } from './TGeometry.js'
+import TGeometry from './TGeometry.js'
 
 export default class CGeometry extends Classes.BaseJSON {
-  @Decorators.Property.Property
-  type?: TGeometry
-  @Decorators.Property.Property
-  coordinates?: number[]
+    @Decorators.Property.Property
+    type?: TGeometry
+    @Decorators.Property.Property
+    coordinates?: number[]
 
-  static init(type?: TGeometry, coordinates?: number[], id?: string, timestamp?: number): CGeometry {
-    // eslint-disable-next-line prefer-rest-params
-    return this.createInitObject(arguments, ['type', 'coordinates', 'id', 'timestamp'])
-  }
+    static init(type?: TGeometry, coordinates?: number[], id?: string, timestamp?: number): CGeometry {
+        // eslint-disable-next-line prefer-rest-params
+        return this.createInitObject(arguments, ['type', 'coordinates', 'id', 'timestamp'])
+    }
 }
